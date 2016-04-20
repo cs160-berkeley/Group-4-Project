5ad3cfe01ce0f1b6dd7783924a6f97a516e7d247
Sarah Au <sau@berkeley.edu>
2016-04-20 01:24:50 -0700

    pollen count retrieved

---
+++ ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/WeatherProgressActivity.java
@@ -0,0 +1,154 @@
+package ea.scratchthathabit;
+
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Sarah on 4/19/2016.
+ */
+public class WeatherProgressActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+//    String url = "http://api.wunderground.com/api/959ec4bfbed19093/conditions/q/CA/San_Francisco.json";
+//    String API_key = "54e774d44be8257d549b05d639d5af57";
+    String mLatitude;
+    String mLongitude;
+
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_progress);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("T", "Connected to Google Maps API");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location != null) {
+            mLatitude = String.valueOf(location.getLatitude());
+            mLongitude = String.valueOf(location.getLongitude());
+            Log.d("T", String.format("Latitude is: %s, longitude is: %s ", mLatitude, mLongitude));
+            new getZipTask().execute();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private class getZipTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String getZipUrl = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + mLatitude + "," + mLongitude;
+            System.out.println(getZipUrl);
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL(getZipUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    result.append(line);
+                }
+                urlConnection.disconnect();
+                String mZip = null;
+
+                JSONObject requestBody = new JSONObject(result.toString());
+                JSONArray jsonResults = requestBody.getJSONArray("results");
+                JSONArray addressComponents = jsonResults.getJSONObject(0).getJSONArray("address_components");
+                for (int i = 0; i < addressComponents.length(); i++) {
+                    JSONObject components = addressComponents.getJSONObject(i);
+                    JSONArray types = components.getJSONArray("types");
+
+                    if (types.getString(0).equals("postal_code")) {
+                        mZip = components.getString("short_name");
+                        System.out.println(mZip);
+                    }
+                }
+                if (mZip != null) {
+                    String getPollenURL = "https://www.claritin.com/webservice/allergyforecast.php?zip=" + mZip + "&_=1461103389676";
+                    System.out.println(getPollenURL);
+                    url = new URL(getPollenURL);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    in = new BufferedInputStream(urlConnection.getInputStream());
+                    br = new BufferedReader(new InputStreamReader(in));
+
+                    result = new StringBuilder();
+                    while ((line = br.readLine()) != null) {
+                        result.append(line);
+                    }
+                    urlConnection.disconnect();
+                    result.replace(result.lastIndexOf("\""), result.length(), "");
+                    result.replace(result.indexOf("\""), result.indexOf("\"") + 1, "");
+                    JSONArray array = new JSONArray(result.toString().replaceAll("\\\\", ""));
+                    JSONObject obj = array.getJSONObject(0);
+                    JSONObject pollenForecast = obj.getJSONObject("pollenForecast");
+                    System.out.println(pollenForecast.toString());
+//                    for (int i = 0; i < array.length(); i++) {
+//                        JSONObject obj = array.getJSONObject(i);
+//                        System.out.println(obj.toString());
+////                        if (obj.has("pollenForecast")) {
+////                            System.out.println("True");
+////                        }
+//                    }
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
