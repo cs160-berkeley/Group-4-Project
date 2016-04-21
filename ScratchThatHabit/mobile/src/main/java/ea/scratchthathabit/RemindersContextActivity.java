af48da100d1ef4711177681cd241c8c3d45bfdba
Sarah Au <sau@berkeley.edu>
2016-04-20 18:35:05 -0700

    reminders linked to lists

--- ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersContextActivity.java
+++ ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersContextActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,31 +19,65 @@ public class RemindersContextActivity extends AppCompatActivity {
 
     ImageButton btn;
     Context ctx;
+    private GestureDetectorCompat mDetector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         System.out.println("Reminders context activity started");
-        super.onCreate(savedInstanceState   );
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reminders_context);
-        btn = (ImageButton) findViewById(R.id.btn);
-        ctx = this;
-        btn.setOnLongClickListener(new View.OnLongClickListener() {
-
-            @Override
-            public boolean onLongClick(View v) {
-                Intent intent = new Intent(ctx, RemindersTimeActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                return true;
-            }
-        });
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, RemindersActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        Log.d("Gestures", "in onTouchEvent");
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG, "onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent event) {
+            Intent sendIntent = new Intent(getBaseContext(), RemindersActivity.class);
+            startActivity(sendIntent);
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Intent sendIntent = new Intent(getBaseContext(), Lists.class);
+            startActivity(sendIntent);
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+//            Intent sendIntent = new Intent(getBaseContext(), WeatherActivity.class);
+//            startActivity(sendIntent);
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent ev) {
+            Intent sendIntent = new Intent(getBaseContext(), RemindersTimeActivity.class);
+            startActivity(sendIntent);
+        }
     }
 
 }
--- ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersTimeActivity.java
+++ ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersTimeActivity.java
@@ -1,46 +1,72 @@
 package ea.scratchthathabit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
 /**
  * Created by Sarah on 4/18/2016.
  */
-public class RemindersTimeActivity extends AppCompatActivity {
+public class RemindersTimeActivity extends Activity {
 
-    ImageButton btn;
-    Context ctx;
+    private GestureDetectorCompat mDetector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        System.out.println("Reminders Time Activity started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders_time);
-
-        ctx = this;
-        btn = (ImageButton) findViewById(R.id.btn);
-
-        btn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Intent intent = new Intent(ctx, RemindersContextActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                return true;
-            }
-        });
-
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(this, RemindersActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        Log.d("Gestures", "in onTouchEvent");
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG, "onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent event) {
+            Intent sendIntent = new Intent(getBaseContext(), RemindersActivity.class);
+            startActivity(sendIntent);
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Intent sendIntent = new Intent(getBaseContext(), Lists.class);
+            startActivity(sendIntent);
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent ev) {
+            Intent sendIntent = new Intent(getBaseContext(), RemindersContextActivity.class);
+            startActivity(sendIntent);
+        }
     }
 }
