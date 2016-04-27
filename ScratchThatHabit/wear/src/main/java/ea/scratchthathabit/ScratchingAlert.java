7c4d5276566ba64aa8df6d790bf14bbda24424df
Simon Bonzon <simonbonzon@berkeley.edu>
2016-04-26 21:51:17 -0700

    Implemented Wear
    Almost fully implemented wear, just need to push reminders from mobile,
    everything else is done

--- ScratchThatHabit/wear/src/main/java/ea/scratchthathabit/ScratchingAlert.java
+++ ScratchThatHabit/wear/src/main/java/ea/scratchthathabit/ScratchingAlert.java
@@ -3,51 +3,34 @@ package ea.scratchthathabit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Simon Bonzon on 4/18/2016.
  */
 public class ScratchingAlert extends MainActivity {
 
-    private GestureDetectorCompat mDetector;
+    private Button mYes;
+    private Button mNo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scratching_alert);
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event){
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onDown: " + event.toString());
-            return true;
-        }
-
-        @Override
-        public boolean onDoubleTap(MotionEvent event) {
-            Intent sendWearIntent = new Intent(getBaseContext(), MainActivity.class);
-            startActivity(sendWearIntent);
-            return true;
-        }
-
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent event) {
-            Intent sendWearIntent = new Intent(getBaseContext(), ScratchingSolution.class);
-            startActivity(sendWearIntent);
-            return true;
-        }
+        mYes = (Button) findViewById(R.id.yes);
+        mNo = (Button) findViewById(R.id.no);
+        mYes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent sendWearIntent = new Intent(getBaseContext(), ScratchingSolution.class);
+                startActivity(sendWearIntent);
+            }
+        });
+        mNo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent sendWearIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(sendWearIntent);
+            }
+        });
     }
 }
--- ScratchThatHabit/wear/src/main/java/ea/scratchthathabit/ScratchingDistraction.java
+++ ScratchThatHabit/wear/src/main/java/ea/scratchthathabit/ScratchingDistraction.java
@@ -1,46 +1,66 @@
 package ea.scratchthathabit;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Simon Bonzon on 4/18/2016.
  */
 public class ScratchingDistraction extends ScratchingSolution {
 
-    private GestureDetectorCompat mDetector;
+    private Button mNext;
+    private Button mBack;
+    private int step;
+    private ImageView mImage;
+    private TextView mStep;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scratching_distraction);
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event){
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onDown: " + event.toString());
-            return true;
-        }
-
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent event) {
-            Intent sendWearIntent = new Intent(getBaseContext(), ScratchingResolution.class);
-            startActivity(sendWearIntent);
-            return true;
-        }
+        mNext = (Button) findViewById(R.id.next);
+        mBack = (Button) findViewById(R.id.back);
+        mImage = (ImageView) findViewById(R.id.instruction);
+        mStep = (TextView) findViewById(R.id.step);
+        step = 1;
+        mNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(step == 17) {
+                    Intent sendWearIntent = new Intent(getBaseContext(), ScratchingResolution.class);
+                    startActivity(sendWearIntent);
+                } else {
+                    step++;
+                    String imageName = "step_" + step;
+                    int id = getResources().getIdentifier("ea.scratchthathabit:drawable/" + imageName,
+                            null, null);
+                    mImage.setImageResource(id);
+                    mStep.setText("Step " + step + ":");
+                }
+            }
+        });
+        mBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(step == 1) {
+                    Intent sendWearIntent = new Intent(getBaseContext(), ScratchingSolution.class);
+                    startActivity(sendWearIntent);
+                } else {
+                    step--;
+                    String imageName = "step_" + step;
+                    int id = getResources().getIdentifier("ea.scratchthathabit:drawable/" + imageName,
+                            null, null);
+                    mImage.setImageResource(id);
+                    mStep.setText("Step " + step + ":");
+                }
+            }
+        });
     }
 }
