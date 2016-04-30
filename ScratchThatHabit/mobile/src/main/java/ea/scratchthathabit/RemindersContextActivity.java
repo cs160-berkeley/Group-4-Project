8ab52cd79d19d74c6031ac2e82019956ef7c1f51
Sarah Au <sau@berkeley.edu>
2016-04-29 17:19:53 -0700

    context reminders layout finished

--- ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersContextActivity.java
+++ ScratchThatHabit/mobile/src/main/java/ea/scratchthathabit/RemindersContextActivity.java
@@ -1,5 +1,6 @@
 package ea.scratchthathabit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,74 +10,193 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 /**
  * Created by Sarah on 4/18/2016.
  */
-public class RemindersContextActivity extends AppCompatActivity {
+public class RemindersContextActivity extends Activity {
 
-    ImageButton btn;
-    Context ctx;
-    private GestureDetectorCompat mDetector;
+    String activity = "RemindersContext";
+
+    ReminderClass Reminder;
+
+    EditText nameInput;
+    EditText addressInput;
+
+    ImageButton save;
+    ImageButton close;
+
+    ImageButton Sunday;
+    ImageButton Monday;
+    ImageButton Tuesday;
+    ImageButton Wednesday;
+    ImageButton Thursday;
+    ImageButton Friday;
+    ImageButton Saturday;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        System.out.println("Reminders context activity started");
+        Log.d(activity, activity + " activity started.");
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reminders_context);
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        Reminder = new ReminderClass();
+
+        nameInput = (EditText) findViewById(R.id.RNameInput);
+        addressInput = (EditText) findViewById(R.id.address_input);
+
+
+        //GETDAYS:
+        Sunday = (ImageButton) findViewById(R.id.Sunday);
+        Sunday.setTag(R.drawable.swhite);
+        Monday = (ImageButton) findViewById(R.id.Monday);
+        Monday.setTag(R.drawable.mwhite);
+        Tuesday = (ImageButton) findViewById(R.id.Tuesday);
+        Tuesday.setTag(R.drawable.twhite);
+        Wednesday = (ImageButton) findViewById(R.id.Wednesday);
+        Wednesday.setTag(R.drawable.wwhite);
+        Thursday = (ImageButton) findViewById(R.id.Thursday);
+        Thursday.setTag(R.drawable.twhite);
+        Friday = (ImageButton) findViewById(R.id.Friday);
+        Friday.setTag(R.drawable.fwhite);
+        Saturday = (ImageButton) findViewById(R.id.Saturday);
+        Saturday.setTag(R.drawable.swhite);
+
+        View.OnClickListener SunbuttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Sunday.getTag() == ((R.drawable.swhite))) {
+                    Sunday.setBackgroundResource(R.drawable.sgreen);
+                    Sunday.setTag(R.drawable.sgreen);
+                    Reminder.addRDay("Sunday");
+                }
+                else {
+                    Sunday.setBackgroundResource(R.drawable.swhite);
+                    Sunday.setTag(R.drawable.swhite);
+                    Reminder.removeRDay("Sunday");
+                }
+            }
+        };
+        View.OnClickListener MBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Monday.getTag() == ((R.drawable.mwhite))) {
+                    Monday.setBackgroundResource(R.drawable.mgreen);
+                    Monday.setTag(R.drawable.mgreen);
+                    Reminder.addRDay("Monday");
+                }
+                else {
+                    Monday.setBackgroundResource(R.drawable.mwhite);
+                    Monday.setTag(R.drawable.mwhite);
+                    Reminder.removeRDay("Monday");
+                }
+            }
+        };
+        View.OnClickListener TuBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Tuesday.getTag() == ((R.drawable.twhite))) {
+                    Tuesday.setBackgroundResource(R.drawable.tgreen);
+                    Tuesday.setTag(R.drawable.tgreen);
+                    Reminder.addRDay("Tuesday");
+                }
+                else {
+                    Tuesday.setBackgroundResource(R.drawable.twhite);
+                    Tuesday.setTag(R.drawable.twhite);
+                    Reminder.removeRDay("Tuesday");
+                }
+            }
+        };
+        View.OnClickListener WBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Wednesday.getTag() == ((R.drawable.wwhite))) {
+                    Wednesday.setBackgroundResource(R.drawable.wgreen);
+                    Wednesday.setTag(R.drawable.wgreen);
+                    Reminder.addRDay("Wednesday");
+                }
+                else {
+                    Wednesday.setBackgroundResource(R.drawable.wwhite);
+                    Wednesday.setTag(R.drawable.wwhite);
+                    Reminder.removeRDay("Wednesday");
+                }
+            }
+        };
+        View.OnClickListener THBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Thursday.getTag() == ((R.drawable.twhite))) {
+                    Thursday.setBackgroundResource(R.drawable.tgreen);
+                    Thursday.setTag(R.drawable.tgreen);
+                    Reminder.addRDay("Thursday");
+                }
+                else {
+                    Thursday.setBackgroundResource(R.drawable.twhite);
+                    Thursday.setTag(R.drawable.twhite);
+                    Reminder.removeRDay("Thursday");
+                }
+            }
+        };
+        View.OnClickListener FBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Friday.getTag() == ((R.drawable.fwhite))) {
+                    Friday.setBackgroundResource(R.drawable.fgreen);
+                    Friday.setTag(R.drawable.fgreen);
+                    Reminder.addRDay("Friday");
+                }
+                else {
+                    Friday.setBackgroundResource(R.drawable.fwhite);
+                    Friday.setTag(R.drawable.fwhite);
+                    Reminder.removeRDay("Friday");
+                }
+            }
+        };
+        View.OnClickListener SatBL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((Integer)Saturday.getTag() == ((R.drawable.swhite))) {
+                    Saturday.setBackgroundResource(R.drawable.sgreen);
+                    Saturday.setTag(R.drawable.sgreen);
+                    Reminder.addRDay("Saturday");
+                }
+                else {
+                    Saturday.setBackgroundResource(R.drawable.swhite);
+                    Saturday.setTag(R.drawable.swhite);
+                    Reminder.removeRDay("Saturday");
+                }
+            }
+        };
+
+        save = (ImageButton) findViewById(R.id.btn_save);
+        close = (ImageButton) findViewById(R.id.btn_close);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSave();
+            }
+        });
+
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(this, RemindersActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+    public void onSave() {
+
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        Log.d("Gestures", "in onTouchEvent");
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
+    public void onClose() {
+
     }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onDown: " + event.toString());
-            return true;
-        }
-
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent event) {
-            Intent sendIntent = new Intent(getBaseContext(), RemindersActivity.class);
-            startActivity(sendIntent);
-            return true;
-        }
-
-        @Override
-        public boolean onDoubleTapEvent(MotionEvent e) {
-            Intent sendIntent = new Intent(getBaseContext(), ListsActivity.class);
-            startActivity(sendIntent);
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//            Intent sendIntent = new Intent(getBaseContext(), WeatherActivity.class);
-//            startActivity(sendIntent);
-            return true;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent ev) {
-            Intent sendIntent = new Intent(getBaseContext(), RemindersTimeActivity.class);
-            startActivity(sendIntent);
-        }
-    }
 
 }
